3e4d45315d234d447131006224a2bfe61099417b
Avinash Panchal <88651615+avinashpanchal123@users.noreply.github.com>
2021-12-29 20:59:58 +0530

    react intro assignment completed

--- learning/src/App.js
+++ learning/src/App.js
@@ -1,25 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+// import { useRef, useRef, useState } from 'react';
+import { buildQueries } from "@testing-library/react";
 
-function App() {
+function App({ nums }) {
+  const style = {
+    color: "red",
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+      {/* <input 
+      onInput={(e) => {
+        // console.log(inputRef.current.value);
+      }}
+      placeholder="onBlur is triggered when you click this input and then you click outside of it."
+    />
+    <button>print</button>
+    <img  src={logo}></img> */}
+      <Heading text={"Mobile Operating System"} />
+      <Disc text={"Android"} />
+      <Disc text={"Blackberry"} />
+      <Disc text={"iPhone"} />
+      <Disc text={"Windows Phone"} />
+
+      <Heading text={"Mobile Manufacturers"} />
+      <Square text={"Samsung"}/>
+      <Square text={"HTC"}/>
+      <Disc text={"Micromax"} />
+      <Circle text={"Micromax"} />
     </div>
   );
+}
+
+function Heading({ text }) {
+  return <h1 style={{
+    paddingLeft :"30%"
+  }}>{text}</h1>;
+}
+
+function Disc({ text }) {
+ 
+  return (
+    <div style={{
+      paddingLeft :"30%"
+    }}>
+      <ul>
+        <li>{text}</li>
+      </ul>
+    </div>
+  );
+}
+
+function Square ({text}){
+  return (
+    <div style={{
+      paddingLeft :"30%"
+    }}>
+       <ul>
+        <li style={{listStyleType: "square"}}>{text}</li>
+      </ul>
+    </div>
+  )
+}
+function Circle ({text}){
+  return (
+    <div style={{
+      paddingLeft :"30%"
+    }}>
+       <ul>
+        <li style={{listStyleType: "circle"}}>{text}</li>
+      </ul>
+    </div>
+  )
 }
 
 export default App;
